de0989f7ed6cc4e7293106efd228f9f8d3b9a1d7
AhlgrenT <theodor17.ahlgren@gmail.com>
2024-11-12 00:23:39 +0100

    Setting up SB, minor success...

---
+++ backend/src/main/java/com/example/ER-to-SQL/ErToSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ER_to_SQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErToSqlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ER_to_SQL/ErToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ER_to_SQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
